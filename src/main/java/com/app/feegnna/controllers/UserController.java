fbff1318123dd3554acdbba2bdf79333f1402fdc
Babacar Faye <faye.babacar78@gmail.com>
2022-01-12 04:25:57 +0000

    update user method

--- src/main/java/com/app/feegnna/controllers/UserController.java
+++ src/main/java/com/app/feegnna/controllers/UserController.java
@@ -5,10 +5,7 @@ import com.app.feegnna.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -29,5 +26,19 @@ public class UserController {
         UserDetails user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Optional<User> optionalUser = userRepository.findByUsername(user.getUsername());
         return optionalUser.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Optional<User> optionalUser = userRepository.findByUsername(userDetails.getUsername());
+        return optionalUser.map(user1 -> {
+            user1.setUsername(user.getUsername());
+            user1.setPrenom(user.getPrenom());
+            user1.setNom(user.getNom());
+            user1.setAdresse(user.getAdresse());
+            user1.setTelephone(user.getTelephone());
+            return ResponseEntity.ok().body(user1);
+        }).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
